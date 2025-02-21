75a56777d308c131cd6c4c7d539f76607b4a9ec4
Joss Appleton-Fox <jossappletonfox@protonmail.com>
2025-02-21 10:03:27 +0000

    Add `Money.equals()`.
    Thanks to our preparatory work in the previous commits, we can make this
    change very easily. We first copy the `equals()` method up into `Money`,
    rename the parameter from `dollar` to `money` for clarity, and then
    delete `Dollar.equals()`.
    
    We can now move on to doing the same for `Franc.equals()`.

--- money.js
+++ money.js
@@ -2,15 +2,15 @@ class Money {
 	constructor(amount) {
 		this.amount = amount;
 	}
+
+	equals(money) {
+		return money.amount === this.amount;
+	}
 }
 
 class Dollar extends Money {
 	times(multiplier) {
 		return new Dollar(this.amount * multiplier);
-	}
-
-	equals(dollar) {
-		return dollar.amount === this.amount;
 	}
 }
 
